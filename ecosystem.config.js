50f67c9b67ae82e7b20bd382c0ce1a9444272584
sobrien <ecosean1@gmail.com>
2023-08-13 12:41:05 -0700

    eco v9

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
-        script: 'npm run build && node .output/server/index.mjs ',
+        script: './.output/server/index.mjs',
       }
     ]
   }
