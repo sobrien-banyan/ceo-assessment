17f95e0a1111792ae970bf0518269b60e1471340
sobrien <ecosean1@gmail.com>
2023-08-13 11:22:14 -0700

    eco v6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
     apps: [
       {
         name: 'ceo-assessment',
-        port: '80',
+        port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
-        script: './node_modules/nuxt/bin/nuxt.js',
+        script: 'npm run dev',
       }
     ]
   }
