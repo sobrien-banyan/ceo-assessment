44074e5c2fa9342de969680561f821b71d5e0e29
sobrien <ecosean1@gmail.com>
2023-08-13 11:16:40 -0700

    eco v4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
-        script: 'nuxt dev'
+        script: './node_modules/nuxt/bin/nuxt.js',
       }
     ]
   }
